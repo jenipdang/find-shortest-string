a9beb95063eb2f7f577418cd42a76b063a547708
Jeni Dang <jenipdang@yahoo.com>
2022-08-04 14:43:19 -0700

    add solution to ruby

--- ruby/find_shortest_string.rb
+++ ruby/find_shortest_string.rb
@@ -1,5 +1,7 @@
+require 'benchmark'
 def find_shortest_string(arr)
   # type your code in here
+  arr.min_by(&:length)
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -19,7 +21,20 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 
   # BENCHMARK HERE
+  puts 
+ 
+  startTime = Time.now
+
+  1000.times do
+    find_shortest_string(['cat', 'hi', 'dog', 'an'])
+  end
+
+  averageTime = (Time.now - startTime) / 1000
+  puts averageTime
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+##1. Iterate over array 
+##2. Compare length of the string in the array 
+##3. Return smallest
